a2f7460c3dd00c41fac6216f6f61dc434c2d40e5
Hakomoto <antosha.hakomoto@gmail.com>
2019-01-18 02:19:19 +0300

    f8_space1 start

---
+++ FUPE-Core/f8_space1.h
@@ -0,0 +1,8 @@
+#pragma once
+//************************************************
+//* Copyright ALTGAS Pte. Ltd. 2019
+//* All rights reserved.
+//*
+//* ALTGAS Pte. Ltd. | Hakomoto
+//************************************************
+// FILE AUTOMATICALLY GENERATED -- DO NOT EDIT
